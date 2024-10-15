6e03092cd2b3dc9e2522e3727255b9ded6fcc898
jorgeli <jorge.f.alviarez@gmail.com>
2024-10-16 00:08:08 +0900

    Rename README.me.sh to README.me

--- README.me.sh
+++
@@ -1,5 +0,0 @@
-docker build -t [REGION]-docker.pkg.dev/[PROJECT_ID]/[REPOSITORY]/[IMAGE_NAME]:[TAG] .
-
-docker push [REGION]-docker.pkg.dev/[PROJECT_ID]/[REPOSITORY]/[IMAGE_NAME]:[TAG]
-
-gcloud run deploy [SERVICE_NAME] --image [REGION]-docker.pkg.dev/[PROJECT_ID]/[REPOSITORY]/[IMAGE_NAME]:[TAG] --platform managed --allow-unauthenticated --set-env-vars OPENAI_API_KEY="[API_KEY]"
